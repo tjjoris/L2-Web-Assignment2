8c6244f493c8312bc588828b8d8de1c1444d1a00
tjjoris <tjjoris@gmail.com>
2024-07-29 15:59:39 -0400

    can successfull connect using admin_login.php. and it shows a query for all in table logins.

--- admin_login.php
+++ admin_login.php
@@ -1,13 +1,39 @@
 <?php   
-require_once 'admin_login_file.php';
+$servername = "localhost";
+$username = "assign2admin";
+$password = "password";
+$data = "web_assign2";
 
 try {
-    $pdo = new PDO($attr, $usr, $pass, $opts);
+  $conn = new PDO("mysql:host=$servername;dbname=$data", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-catch (PDOException $e){
-    throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
-$query = "SELECT" * FROM logins;
-$result = $pdo->qurey($query);
-echo $result;
+
+$query = "SELECT * FROM logins";
+$report = $conn->query($query);
+// echo $report->fetch();
+// echo htmlspecialchars($report['uname']);
+$result = $report->fetch(PDO::FETCH_ASSOC);
+print_r($result);
+
+
+
+// require_once 'admin_login_file.php';
+// echo $attr;
+// try {
+//     // $pdo = new PDO($attr, $usr, $pass, $opts);
+//     $pdo = new PDO("mysql:host=localhost;dbname=web_assign2", "root", "");
+// }
+// catch (PDOException $e){
+//     // throw new PDOException($e->getMessage(), (int)$e->getCode());
+// }
+// echo $attr;// . "<br>" . $usr . "<br>" . $pass . "<br>" $opts;
+// print_r $pdo;
+// $query = "SELECT * FROM logins";
+// $result = $pdo->qurey($query);
+// echo $result;
 ?>
--- admin_login_file.php
+++ admin_login_file.php
@@ -1,6 +1,12 @@
 <?php //admin login
+ $chrs = "utf8mb4";
  $host = 'localhost';
- $user = 'web2Admin';
+ $usr = 'assign2admin';
  $pass = 'password';
  $data = 'web_assign2';
+ $attr = "mysql:host=$host;dbname=$data;";
+//  $opts = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//  PDO::ATTR_EMULATE_PREPARES => false,
+];
  ?>
