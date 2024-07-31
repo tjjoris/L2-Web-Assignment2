fcdc92d16521c06b0f266c2c029aaef413d12aa4
tjjoris <tjjoris@gmail.com>
2024-07-31 00:24:05 -0400

    deleted files that don't belong, and changed admin_login_file.php to login_file.php

--- login_file.php
+++ login_file.php
@@ -1,10 +1,13 @@
 <?php //admin login
  $chrs = "utf8mb4";
- $host = 'localhost';
- $usr = 'assign2admin';
- $pass = 'password';
- $data = 'web_assign2';
+//  $usr = 'assign2admin';
+//  $pass = 'password';
  $attr = "mysql:host=$host;dbname=$data;";
+ $host="localhost";
+ $user="root";
+ $pass="";
+ $db="web_assign2";
+
 //  $opts = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 //  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 //  PDO::ATTR_EMULATE_PREPARES => false,
--- test.js
+++
@@ -1,3 +0,0 @@
-console.log(document.getElementsByTagName("li"));
-console.log(document.getElementById("li"));
-console.log(document.quarySelector("li"));
