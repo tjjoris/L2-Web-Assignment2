4f3f01a13a9f0ab78b99609a5bfe44bcf9449fbd
tjjoris <tjjoris@gmail.com>
2024-08-02 00:14:17 -0400

    was able to successfull insert into the posts child table with a foreign key form the last inserted row.

--- submit-new-thread.php
+++ submit-new-thread.php
@@ -22,11 +22,10 @@ if ((!isset($_POST)) ){
 //post exists
 else {
     //thread title exists
-    if ((isset($_POST['threadtitle']))) {
-        echo "tread title isset";
-        //thread title is not empty
-        if (!empty($_POST['threadtitle'])) {
+    if ((isset($_POST['threadtitle'])) && (!empty($_POST['threadtitle']))
+        && (isset($_POST['content-container'])) && (!empty($_POST['content-container']))) {
             $threadtitle=$_POST['threadtitle'];
+            $content_container=$_POST['content-container'];
             echo "thread title is not empty";
             echo $_POST['threadtitle'];
             
@@ -41,29 +40,41 @@ else {
             //else, you are connected.
             else {
                 //add to thread query
-                $query_insert="INSERT INTO threads (thread_name) VALUES ('$threadtitle')";
-                // $insert_query="INSERT INTO logins (uname, email, password) 
-                //         VALUES ('$uname', '$email', '$hash')";
-                
-                //insert thread into table
-                $result_set = mysqli_query($conn, $query_insert);
-                //get last surrogate key
-                // $result_set = mysqli_query($conn, $query_insert);
+                $qry_insert_threads="INSERT INTO threads (thread_name) VALUES ('$threadtitle')";
                 
                 //get last surrogate key query
-                $query_get_last_key="SELECT LAST_INSERT_ID()";
+                $qry_thread_key="SELECT LAST_INSERT_ID() FROM threads";
+
+                
+                
+                //insert thread into table
+                $result_set = mysqli_query($conn, $qry_insert_threads);
+
+                //get thread key
+                // $thread_key_obj = mysqli_query($conn, $qry_thread_key);
+                // //get the result from the query.
+                // $key_result = mysqli_fetch_assoc($thread_key_obj);
+                // //get the value of the key attribute form the result.
+                // $thread_key_value = $key_result['0'];
+
+                $last_id = $conn->insert_id;
+
+                echo "<br>$last_id<br>";
+                
+                $qry_insert_posts="INSERT INTO posts (thread_id, login_id, post_time, message) 
+                        VALUES ('$last_id', '1', NOW(), '$content_container')";
+
+                //insert into posts
+                $result_set = mysqli_query($conn, $qry_insert_posts);
+                
                 
 
             }
 
         } else {
-            echo "thread title is empty";
+            echo "thread values not set";
         }
     }
-    else {
-    echo "post var not set";
-    }
-}
 }
 
 
