029b111f17eb12a401c5b827f74f91f68078bbb2
tjjoris <tjjoris@gmail.com>
2024-08-07 13:56:08 -0400

    login, register, logout
    login and register pages linked as well as logout page.
    includes CSS for login and register pages.

---
+++ index3.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- the container to register -->
+     <!-- register is sent to register2.php as a POST where uname = user name
+      pword = password, and email = email. -->
+    <div id="register-container">
+        <form method="post" action="register2.php">
+            <div>
+            <label name="uname">User Name</label>
+            <input type="text" name="uname" id="uname">
+            </div>
+            <div>
+            <label name="email">Email</label>
+            <input type="text" name="email" id="email">
+            </div>
+            <div>
+            <label name="pword">Password</label>
+            <input type="text" name="pword" id="pword">
+            </div>
+            <div>
+                <button name="submit" id="submit">register</button>
+            </div>
+        </form>
+    </div>
+
+    <!-- the container to connect -->
+     <!-- connect is sent to connect2.php as POST where uname = user name 
+      pword = password, and email = email. -->
+    <div id="connect-container">
+        <form method="post" action="connect2.php">
+            <div>
+            <label name="uname">User Name</label>
+            <input type="text" name="uname" id="uname">
+            </div>
+            <div>
+            <label name="email">Email</label>
+            <input type="text" name="email" id="email">
+            </div>
+            <div>
+            <label name="pword">Password</label>
+            <input type="text" name="pword" id="pword">
+            </div>
+            <div>
+                <button name="submit" id="submit">connect</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
--- logout.php
+++ logout.php
@@ -2,5 +2,6 @@
 <?php
 require_once "start_session.php";
 $_SESSION['logged_in'] = FALSE;
-require_once "start_session.php";
+// require_once "start_session.php";
+header ("Location: index.php");
 ?>
