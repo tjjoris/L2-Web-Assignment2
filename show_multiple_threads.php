157ab85e426a4465fea7104c282aa2ed93c7a635
tjjoris <tjjoris@gmail.com>
2024-08-07 17:58:21 -0400

    show_multiple_threads.php showing first 5 threads

--- show_multiple_threads.php
+++ show_multiple_threads.php
@@ -26,6 +26,11 @@ if (!isset($_SESSION)){
     echo "session is not set";
 }
 else {
+    $main_threads_page_number = 0;
+    // if ((!isset($_SESSION['main_threads_page_number'])) && (empty($_SESSION['main_threads_page_number']))){
+    //     $main_threads_page_number = $_SESSION['main_threads_page_number'];
+    // }
+
     if ((!isset($_SESSION['show_threads'])) && (empty($_SESSION['show_threads'])))
     {
         echo "_SESSION show threads empty";
@@ -35,53 +40,63 @@ else {
 
         // echo "showing threads";
 
+        $count = 0;
         foreach ($result_set as $row) {
             $current_thread_id = $row['id'];
+            if (($count >= 0) && ($count < 5)) {
+        // }
+        // for ($i = 2; $i < min(21, count($row)); $i++) {
+        //     // $rows[$i]['id']
+        //     echo $row[$i]['id'];
+            // $current_thread_id = $row[$i]['id'];
+        
             // echo $row['id'];
 
         //create a new mysqli connection
         // $conn = new mysqli($host,$user,$pass,$db,$port);
 
-//if sqli connection error print error message.
-if ($conn->connect_error){
-    echo "error here";
-    echo "failed to connect to db".$conn->connect_error;
-}
-//else, you are connected.
-else {
-    
-    $qry_one_thread="SELECT threads.id AS id, thread_name, posts.post_time AS post_time, logins.uname AS uname FROM threads LEFT JOIN posts ON posts.thread_id = threads.id LEFT JOIN logins ON logins.id = posts.login_id WHERE threads.id = $current_thread_id";
-
-        //run query to get posts
-        $result_set = mysqli_query($conn, $qry_one_thread);
-        $first_record = mysqli_fetch_assoc($result_set);
-
-        $thread_id = $first_record['id'];
-        $thread_name = $first_record['thread_name'];
-        $post_time = $first_record['post_time'];
-        $uname = $first_record['uname'];
-
+        //if sqli connection error print error message.
+        if ($conn->connect_error){
+            echo "error here";
+            echo "failed to connect to db".$conn->connect_error;
+        }
+        //else, you are connected.
+        else {
         
-        echo <<<_END
-        <div class="main-thread">
-            <a href="thread_redirect.php?thread_id=$thread_id">
-            <div class="user-profile">
-                <img src="images/userIcon.png">
-                <div>
-                    <p>$uname / author</p>
-                    <span class="when-posted">$post_time</span>
-                </div>
-            </div>
-            <div class="main-thread-title">
-                    $thread_name
-            </div>
-            </a>
-        </div>  
-        _END;
+        $qry_one_thread="SELECT threads.id AS id, thread_name, posts.post_time AS post_time, logins.uname AS uname FROM threads LEFT JOIN posts ON posts.thread_id = threads.id LEFT JOIN logins ON logins.id = posts.login_id WHERE threads.id = $current_thread_id";
 
+            //run query to get posts
+            $result_set = mysqli_query($conn, $qry_one_thread);
+            $first_record = mysqli_fetch_assoc($result_set);
+
+            $thread_id = $first_record['id'];
+            $thread_name = $first_record['thread_name'];
+            $post_time = $first_record['post_time'];
+            $uname = $first_record['uname'];
+
+            
+            echo <<<_END
+            <div class="main-thread">
+                <a href="thread_redirect.php?thread_id=$thread_id">
+                <div class="user-profile">
+                    <img src="images/userIcon.png">
+                    <div>
+                        <p>$uname / author</p>
+                        <span class="when-posted">$post_time</span>
+                    </div>
+                </div>
+                <div class="main-thread-title">
+                        $thread_name
+                </div>
+                </a>
+            </div>  
+            _END;
+
+            
+            }
+            $count ++;
         }
     }
-        
     }
 }
 ?>
